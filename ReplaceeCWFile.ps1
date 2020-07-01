c48ea8a2b94be2c1d14787466451c578bf46d83d
Suncoast CHC <67698280+suncoast-chc@users.noreply.github.com>
2020-07-01 10:28:23 -0400

    Update ReplaceeCWFile.ps1

--- ReplaceeCWFile.ps1
+++ ReplaceeCWFile.ps1
@@ -1,3 +1,19 @@
-﻿Set-ExecutionPolicy -ExecutionPolicy Unrestricted
-$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/bwindham-suncoast/Public/master/ReplaceeCWFile.ps1
-Invoke-Expression $($ScriptFromGitHub.Content)
+## tests if the path C:\Program Files (x86)\eClinicalworks\ exists ##
+## if the path exists it will copy DentalDevices.xml to that path ##
+## If it doesnt exist, it will write thst the path doesnt exist ##
+
+## variables ##
+$path = "C:\Program Files (x86)\eClinicalWorks"
+$a = test-path -Path $path
+$networkShare = "\\nas\playground\IT\ERCActiveX.ocx"
+$destination = "C:\Program Files (x86)\eClinicalWorks\ERCActiveX.ocx"
+
+## test the path - if true ##
+if ($a -eq "True") {
+copy-item $networkShare -Destination $destination
+}
+
+## test the path - if false ##
+else {
+write-host "$path does not exist on $env:computername"
+}
