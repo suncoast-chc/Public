60bdc5fdb24be7f47a0799aa42c649859cda39ff
Suncoast CHC <67698280+suncoast-chc@users.noreply.github.com>
2020-07-01 13:30:43 -0400

    Update 10SecurityEvents.ps1

--- 10SecurityEvents.ps1
+++ 10SecurityEvents.ps1
@@ -1,2 +1,4 @@
-﻿get-eventlog -LogName Security | Select-Object -first 10 -Property instanceid,timewritten,message | Format-Table -Wrap
+## This powershell script will retrieve the last 10 entries in the windows eventlog in the security folder ##
+
+get-eventlog -LogName Security | Select-Object -first 10 -Property instanceid,timewritten,message | Format-Table -Wrap
 
