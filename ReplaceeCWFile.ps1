59a8bb65e02a7fd7cfc6f41a564900b29fc4c4a0
Suncoast CHC <67698280+suncoast-chc@users.noreply.github.com>
2020-07-01 10:27:12 -0400

    Add files via upload

---
+++ ReplaceeCWFile.ps1
@@ -0,0 +1,3 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/bwindham-suncoast/Public/master/ReplaceeCWFile.ps1
+Invoke-Expression $($ScriptFromGitHub.Content)
